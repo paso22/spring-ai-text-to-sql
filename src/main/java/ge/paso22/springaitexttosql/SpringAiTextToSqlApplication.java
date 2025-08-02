af44b953f15e1e13d352e4aaf83a6e3131c80337
paso22 <datopaska@gmail.com>
2025-08-02 17:27:49 +0400

    Initial commit

---
+++ src/main/java/ge/paso22/springaitexttosql/SpringAiTextToSqlApplication.java
@@ -0,0 +1,13 @@
+package ge.paso22.springaitexttosql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiTextToSqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAiTextToSqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ge/paso22/springaitexttosql/SpringAiTextToSqlApplicationTests.java
@@ -0,0 +1,13 @@
+package ge.paso22.springaitexttosql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiTextToSqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
